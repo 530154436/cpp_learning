1d1a7135cf72ea0abdde5591de95069fbe57d778
郑楚彬 <530154436@qq.com>
2020-09-10 09:37:21 +0800

    Add template

--- data_struct/wangdao/ch03_stack_queue/exercises.hpp
+++ data_struct/wangdao/ch03_stack_queue/exercises.hpp
@@ -5,5 +5,9 @@
 #define TIANQINDATASTRUCT_EXERCISES_HPP
 
 
+/*
+ * 练习 3.1 栈 p71
+ */
+int dc(LinkList L, int n);// 4. 设单链表的表头指针为 L，结点结构由 data 和 next 两个域构成，其中 data 域为字 符型 。 试设计算法判断该链表的全部 n 个字符是否中心对称 。 例如 xyx、 xyyx 都是中 心对称。
 
 #endif //TIANQINDATASTRUCT_EXERCISES_HPP
---
+++ data_struct/wangdao/templates/LinkList.hpp
@@ -0,0 +1,41 @@
+//
+// Created by 郑楚彬 on 2020/9/3.
+//
+#ifndef TIANQINDATASTRUCT_LINKLIST_HPP
+#define TIANQINDATASTRUCT_LINKLIST_HPP
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/**
+ * 单链表 (带头结点)
+ */
+typedef int ElemType;           // 元素数据类型
+typedef struct LNode{
+    ElemType data;              // 数据域
+    struct LNode* next;         // 指针域
+}LNode, *LinkList;
+
+/**
+ * 单链表的基本操作
+ */
+LinkList List_HeadInsert(LinkList &L, int a[], int n);  // 采用头插法建立单链表
+LinkList List_TailInsert(LinkList &L, int a[], int n);  // 采用尾插法建立单链表
+LNode *GetElem(LinkList L, int i);                      // 按位查找操作。获取表 L 中第 i 个位置的元素的值。
+LNode *LocateElem(LinkList L, ElemType e);              // 按值查找操作。在表 L 中查找具有给定关键宇值的元素。
+void PrintLinkList(LinkList L);                         // 输出操作。按前后顺序输出线性表 L 的所有元素值。
+int Length(LinkList L);                                 // 求表长。返回线性表 L 的长度，即 L 中数据元素的个数。
+
+/**
+ * 双链表
+ */
+typedef struct DNode{           // 定义双链表结点类型
+    ElemType data ;             // 数据域
+    struct DNode *prior,*next;  // 前驱和后继
+
+    int freq;                   // 频度域 => 练习 2.2线性表的链式表示第20题 p40-p44
+}DNode, *DLinkList ;
+
+
+#endif //TIANQINDATASTRUCT_SQLIST_HPP
